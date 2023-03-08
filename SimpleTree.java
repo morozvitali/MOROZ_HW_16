eb249577f36057c7d50872c31ecc991abdcb6d63
morozvitali <80890701+morozvitali@users.noreply.github.com>
2023-03-08 17:40:43 +0200

    init proj ##2

--- SimpleTree.java
+++ SimpleTree.java
@@ -1,20 +1,20 @@
 public class SimpleTree {
-    private Node root;
-    public void add(int value) {
+    private Node root; // ключовий нод
+    public void add(int value) { // додати новий нод зі значенням value
         root = addRecursive(root, value);
     }
 
     private Node addRecursive(Node current, int value) {
-        if (current == null) {
+        if (current == null) { // якщо
             return new Node(value);
         }
 
-        if (value < current.value) {
+        if (value < current.value) { // рекурсивна перевірка, менше або більше
             current.left = addRecursive(current.left, value);
         } else if (value > current.value) {
             current.right = addRecursive(current.right, value);
         } else {
-            // value already exists
+            // значення вже існує
             return current;
         }
 
